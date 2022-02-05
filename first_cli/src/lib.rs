c7ab5cc80e67fe40b0e8888cbe031eab06f03194
SomeSomewhere <cygnusborjigin@gmail.com>
2022-02-05 08:48:40 -0500

    first cli
    search function is working

--- first_cli/src/lib.rs
+++ first_cli/src/lib.rs
@@ -1,5 +1,4 @@
 use std::fs;
-use std::process; // for exit the program without panic
 use std::error::Error; // for construction of the error type
 
 pub struct Config {
@@ -16,14 +15,38 @@ impl Config {
          let query = args[1].clone();
          let file_name = args[2].clone();
 
-         return Ok(Config{ query, file_name });
+        return Ok(Config{ query, file_name });
     }
 }
 
 pub fn run ( config: Config ) -> Result<(), Box<dyn Error>>{
      let content = fs::read_to_string(&config.file_name)?;
-     println!("The query is: {}", &config.query);
-     println!("The file name is: {}", &config.file_name);
-     println!("{}", &content);
+
+     for line in search(&config.query, &content) {
+        println!("{}", line);
+     }
+
      return Ok(());
+}
+
+pub fn search <'a> (query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in content.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    return result;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*; // import everything from the parent
+    #[test]
+    fn one_result () {
+        let query = "content";
+        let content = "content of the first test";
+
+        assert_eq!(vec!["content of the first test"], search(query, content));
+    }
 }
