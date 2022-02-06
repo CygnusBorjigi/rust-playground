a5b0a4578980d39d15696e06cb3d7e5ad2e56e40
SomeSomewhere <cygnusborjigin@gmail.com>
2022-02-06 16:41:42 -0500

    txt to hash
    finish file read and string explode

--- txt_to_hash/src/main.rs
+++ txt_to_hash/src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // take and check user input
+    let input_args: Vec<String> = env::args().collect();
+    if input_args.len() < 2 {
+        eprintln!("please enter the file name");
+    } else if input_args.len() > 2 {
+        eprintln!("only one argument is accepted");
+    };
+
+    // search and open file
+    let file_content = fs::read_to_string(&input_args[1]).expect("file cannot be open");
+    println!("{:?}", file_content);
+
+    // parse the file into indivisual words
+    let file_char_list = explode(file_content);
+    println!("{}", file_char_list.len());
+    println!("{:?}", file_char_list);
+    
+}
+
+fn explode (source: String) -> Vec<char> {
+    return source.chars().collect();
 }
