39070b560689738c02a11dcf5557da4f18c8bb6a
SomeSomewhere <cygnusborjigin@gmail.com>
2022-02-06 21:47:17 -0500

    txt to hash
    first pass parser working

--- txt_to_hash/src/main.rs
+++ txt_to_hash/src/main.rs
@@ -16,12 +16,22 @@ fn main() {
     // parse the file into indivisual words
     let file_char_list = explode(file_content);
 
-    let result = lexer(file_char_list);
+    let result =  identify_char_whitespace_linebreak(file_char_list);
     println!("{:?}", result);
 }
 
-fn lexer (source: Vec<char>) -> Element {
-    return Element::Word(String::from("Working"));
+fn identify_char_whitespace_linebreak (source: Vec<char>) -> Vec<Element> {
+    let iter = source.iter();
+    let output: Vec<_> = iter.map(|each| {
+        if each == &' ' {
+            Element::WhiteSpace(1)
+        } else if each == &'\n' {
+            Element::LineBreak
+        } else {
+            Element::Char(*each)
+        }
+    }).collect();
+    return output;
 }
 
 fn explode (source: String) -> Vec<char> {
@@ -31,6 +41,7 @@ fn explode (source: String) -> Vec<char> {
 #[derive(Debug)]
 enum Element {
     Word(String),
+    Char(char),
     Punctuation(char),
     WhiteSpace(i32),
     LineBreak,
