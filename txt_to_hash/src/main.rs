3ce483dcb3656d58f1cb845f6414de41e3bbe69f
SomeSomewhere <cygnusborjigin@gmail.com>
2022-02-09 20:58:55 -0500

    txt to hash
    spearate parser into module

--- txt_to_hash/src/main.rs
+++ txt_to_hash/src/main.rs
@@ -1,15 +1,11 @@
 use std::env;
 mod user_input;
+mod parser;
 
 fn main() {
     let file_content: String = user_input::run(env::args().collect());
-
-    // parse the file into indivisual elements
-    let file_char_list = explode(file_content);
-
-    let first_pass =  identify_char_whitespace_linebreak(file_char_list);
-    let second_pass = char_to_word(first_pass);
-    println!("{:?}", second_pass);
+    let internal = parser::run(file_content);
+    println!("{:?}", internal);
 }
 
 #[derive(Debug)]
@@ -19,93 +15,5 @@ struct HashMap {
     whitespace: i32,
     linebreak: i32,
 }
-
-
-fn char_to_word (source: Vec<Element>) -> Vec<Element> {
-    let mut result = Vec::new();
-    let mut accum = Vec::new();
-    for i in source {
-        match i {
-            Element::Char(ele) => accum.push(ele),
-            Element::Punctuation(ele) => {
-                if &accum.len() != &0 {
-                    let mut new_word = String::from("");
-                    for a in &accum {
-                        new_word.push(*a);
-                    }
-                    result.push(Element::Word(new_word));
-                    accum = Vec::new();
-                } 
-                result.push(Element::Punctuation(ele));
-            },
-            Element::WhiteSpace(ele) => {
-                if &accum.len() != &0 {
-                    let mut new_word = String::from("");
-                    for a in &accum {
-                        new_word.push(*a);
-                    }
-                    result.push(Element::Word(new_word));
-                    accum = Vec::new();
-                } 
-                result.push(Element::WhiteSpace(ele));
-            },
-            Element::LineBreak => {
-                if &accum.len() != &0 {
-                    let mut new_word = String::from("");
-                    for a in &accum {
-                        new_word.push(*a);
-                    }
-                    result.push(Element::Word(new_word));
-                    accum = Vec::new();
-                } 
-                result.push(Element::LineBreak);
-            },
-            Element::Word(ele) => result.push(Element::Word(ele))
-        }
-    }
-    if &accum.len() != &0 {
-        let mut new_word = String::from("");
-        for a in &accum {
-            new_word.push(*a);
-        }
-        result.push(Element::Word(new_word));
-    }
-    return result;
-}
-
-fn identify_char_whitespace_linebreak (source: Vec<char>) -> Vec<Element> {
-    let iter = source.iter();
-    let output: Vec<_> = iter.map(|each| {
-        if each == &' ' {
-            Element::WhiteSpace(1)
-        } else if each == &'\n' {
-            Element::LineBreak
-        } else if is_poun(*each) {
-            Element::Punctuation(*each)
-        } else {
-            Element::Char(*each)
-        }
-    }).collect();
-    return output;
-}
-
-fn is_poun (source: char) -> bool {
-    let target = ",.:;-'";
-    return target.contains(source);
-}
-
-fn explode (source: String) -> Vec<char> {
-    return source.chars().collect();
-}
-
-#[derive(Debug)]
-enum Element {
-    Word(String),
-    Char(char),
-    Punctuation(char),
-    WhiteSpace(i32),
-    LineBreak,
-}
-
 
 
---
+++ txt_to_hash/src/parser.rs
@@ -0,0 +1,93 @@
+
+pub fn run(file_content: String) -> Vec<Element> {
+    let char_list: Vec<char> = explode(file_content);
+    let first_pass = char_whitespace_linebreak(char_list);
+    let second_pass = char_to_word(first_pass);
+    return second_pass;
+}
+
+#[derive(Debug)]
+pub enum Element {
+    Word(String),
+    Char(char),
+    Punctuation(char),
+    Whitespace(i32),
+    Linebreak,
+}
+
+fn explode (source: String) -> Vec<char> {
+    return source.chars().collect();
+}
+
+fn char_whitespace_linebreak (source: Vec<char>) -> Vec<Element> {
+    let iter = source.iter();
+    let output: Vec<Element> = iter.map(|each| {
+        if each == &' ' {
+            Element::Whitespace(1)
+        } else if each == &'\n' {
+            Element::Linebreak
+        } else if is_punctuation(each) {
+            Element::Punctuation(*each)
+        } else {
+            Element::Char(*each)
+        }
+    }).collect();
+    return output;
+}
+
+fn is_punctuation (source: &char) -> bool {
+    let target = ",.:;-'";
+    return target.contains(*source);
+}
+
+fn char_to_word (source: Vec<Element>) -> Vec<Element> {
+    let mut result = Vec::new();
+    let mut accum = Vec::new();
+    for i in source {
+        match i {
+            Element::Char(ele) => accum.push(ele),
+            Element::Punctuation(ele) => {
+                if &accum.len() != &0 {
+                    let mut new_word = String::from("");
+                    for a in &accum {
+                        new_word.push(*a);
+                    }
+                    result.push(Element::Word(new_word));
+                    accum = Vec::new();
+                } 
+                result.push(Element::Punctuation(ele));
+            },
+            Element::Whitespace(ele) => {
+                if &accum.len() != &0 {
+                    let mut new_word = String::from("");
+                    for a in &accum {
+                        new_word.push(*a);
+                    }
+                    result.push(Element::Word(new_word));
+                    accum = Vec::new();
+                } 
+                result.push(Element::Whitespace(ele));
+            },
+            Element::Linebreak => {
+                if &accum.len() != &0 {
+                    let mut new_word = String::from("");
+                    for a in &accum {
+                        new_word.push(*a);
+                    }
+                    result.push(Element::Word(new_word));
+                    accum = Vec::new();
+                } 
+                result.push(Element::Linebreak);
+            },
+            Element::Word(ele) => result.push(Element::Word(ele))
+        }
+    }
+    if &accum.len() != &0 {
+        let mut new_word = String::from("");
+        for a in &accum {
+            new_word.push(*a);
+        }
+        result.push(Element::Word(new_word));
+    }
+    return result;
+}
