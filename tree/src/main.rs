8e531aeb7810ea8230195d1d247c493be27f9bc2
SomeSomewhere <cygnusborjigin@gmail.com>
2022-02-14 22:10:33 -0500

    tree
    finish create method

--- tree/src/main.rs
+++ tree/src/main.rs
@@ -1,3 +1,32 @@
+use std::rc::Rc;
+use std::cell::RefCell;
 fn main() {
-    println!("Hello, world!");
+    let testTree = TreeNode::create(88);
+    println!("{:?}", testTree);
+}
+
+#[derive(Debug)]
+struct TreeNode {
+    data: i64,
+    left: Option<Rc<RefCell<TreeNode>>>,
+    right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    fn create(new_data: i64) -> TreeNode {
+        TreeNode{
+            data: new_data,
+            left: None,
+            right: None
+        }
+    }
+
+    fn insert(&mut self, new_data: i64) -> TreeNode {
+        if self.data < new_data {
+            match self.right {
+                None => self.right = Some(TreeNode::create(new_data)),
+                Some(_) => self.right.insert(new_data),
+            }
+        }
+    }
 }
